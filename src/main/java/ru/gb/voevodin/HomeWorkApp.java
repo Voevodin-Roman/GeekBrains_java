9a5b6ba8655fe1ff308a44287a3b8cb2902d4424
Voevodin_Roman <q556677@yandex.ru>
2022-04-12 14:45:08 +0400

    add  class TheeWords

--- src/main/java/ru/gb/voevodin/HomeWorkApp.java
+++ src/main/java/ru/gb/voevodin/HomeWorkApp.java
@@ -1,6 +1,12 @@
 package ru.gb.voevodin;
 
 public class HomeWorkApp {
+    public static void TheeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
     public static void main(String[] args) {
 
 
