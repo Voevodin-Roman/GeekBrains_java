a025d4479af8d261cccc5d8361caec0864c9ce47
Voevodin_Roman <q556677@yandex.ru>
2022-04-15 11:28:23 +0400

    Add class HomeWorkApp2

---
+++ src/main/java/ru/gb/voevodin/HomeWorkApp2.java
@@ -0,0 +1,20 @@
+package ru.gb.voevodin;
+
+public class HomeWorkApp2 {
+
+
+    public static void main(String[] args) {
+
+    }
+    public static void printTheeWords() {
+    }
+    public static void checkSumSing(){
+
+    }
+    public static void printColor(){
+
+    }
+    public static void compareNumbers(){
+
+    }
+}
