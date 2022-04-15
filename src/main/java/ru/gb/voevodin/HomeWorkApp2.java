a2520ddb25d74275354c705e64f7c0423d78e3dc
Voevodin_Roman <q556677@yandex.ru>
2022-04-15 13:16:11 +0400

    Add homework 2 1-4

--- src/main/java/ru/gb/voevodin/HomeWorkApp2.java
+++ src/main/java/ru/gb/voevodin/HomeWorkApp2.java
@@ -1,20 +1,50 @@
 package ru.gb.voevodin;
 
 public class HomeWorkApp2 {
-
-
     public static void main(String[] args) {
-
+        int a = 9;
+        int b = 2;
+        int year = 1999;
+        String text = "Печатаем текст";
+        System.out.println("Задание 1 - " + checkingTwoNumbers(a , b));
+        realNumberSign(a);
+        System.out.println("Задание 3 - " + numberState(a));
+        printText(a, text);
+        leapYearDefinition(year);
     }
-    public static void printTheeWords() {
+    //Задание 1
+    public static boolean checkingTwoNumbers(int a, int b) {
+        if( a + b >= 10 && a + b <= 20){
+            return true;
+        }else{
+            return false;
+        }
     }
-    public static void checkSumSing(){
-
+    //Задание 2
+    public static void realNumberSign(int a){
+        if(a < 0){
+            System.out.println("Задание 2 - " + "Число отрицательное");
+        }else{
+            System.out.println("Задание 2 - " + "Число положительное");
+        }
     }
-    public static void printColor(){
-
+    //Задание 3
+    public static boolean numberState(int a){
+        if( a < 0){
+            return true;
+        }else {
+            return false;
+        }
     }
-    public static void compareNumbers(){
+    //Задание 4
+    public static void printText(int a, String text){
+        System.out.println("Задание 4 - печатаем переменную " + a + " раз(а):");
+        for (int i = 0; i < a; i++) {
+            System.out.println(text);
+        }
+    }
+    //Задание 5
+    public static void leapYearDefinition(int y){
 
     }
 }
