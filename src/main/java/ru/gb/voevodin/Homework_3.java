ba5e744f41577882ce54c15f89e05a5aef963df5
Voevodin_Roman <q556677@yandex.ru>
2022-04-19 09:45:45 +0400

    Add class Homework-3

---
+++ src/main/java/ru/gb/voevodin/Homework_3.java
@@ -0,0 +1,8 @@
+package ru.gb.voevodin;
+
+public class Homework_3 {
+    public static void main(String[] args) {
+        int[] mas = new int[1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1];
+        System.out.println(mas);
+    }
+}
