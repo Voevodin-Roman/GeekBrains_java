2296a21837be55df1272051bf0c6eecb31c46729
Voevodin_Roman <q556677@yandex.ru>
2022-04-20 17:30:11 +0400

    Add homework3  8

--- src/main/java/ru/gb/voevodin/Homework_3.java
+++ src/main/java/ru/gb/voevodin/Homework_3.java
@@ -1,6 +1,8 @@
 package ru.gb.voevodin;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Stack;
+
 public class Homework_3 {
     public static void main(String[] args) {
 
@@ -36,7 +38,7 @@ public class Homework_3 {
         System.out.println("Задание 3: " + Arrays.toString(c));
 
         //Задание 4
-        int size = 6;
+        int size = 5;
         int[][] d = new int[size][size];
         for (int l = 0; l < d.length; l++) {
             d[l][l] = 1;
@@ -57,23 +59,30 @@ public class Homework_3 {
         Arrays.sort(w);
         int min = w[0];
         int max = w[w.length - 1];
-        System.out.println("Задание 6: Минимальное значение: " + min);
-        System.out.println("Задание 6: Максимальное значение: " + max);
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное значение: " + max);
 
         //Задание 7
-        int[] array7 = {1, 1, 1, 0, 0, 3};
-        //for (int u = 0; u < array7.length; u++) { //Заполняем массив случайными числами
-         //  array7[u] = new Random().nextInt(99);
-        //}
+        int[] array7 = new int[4];
+        for (int u = 0; u < array7.length; u++) { //Заполняем массив случайными числами
+        array7[u] = new Random().nextInt(2);
+        }
         System.out.println("Задание 7: массив " + Arrays.toString(array7));
-        if (checkArrey(array7) == true){
-            System.out.println("Данный массив делится внутри себя на равные части ");
+        if (checkBalance(array7)){
+            System.out.println("Данный массив делится внутри себя на равные части");
         }else{
-            System.out.println("Данный массив не делится внутри себя на равные части ");
+            System.out.println("Данный массив не делится внутри себя на равные части");
         }
 
-
-
+        //Задание 8
+        int[] array8 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int positionShift = 4;
+        System.out.println("Задание 8: Начальный массив " + Arrays.toString(array8));
+        if(positionShift > array8.length -1 ){
+            System.out.println("Ошибка. Заданное смещение <" + positionShift + "> больше длинны масcива");
+        }else {
+            System.out.println("Задание 8: Конечный массив, смещение на " + positionShift + " позиций " + Arrays.toString(mixingValuesInArray(array8, positionShift)));
+        }
     }
 
     //Задание 5
@@ -85,19 +94,42 @@ public class Homework_3 {
             return array;
         }
     //Задание 7
-    public static boolean checkArrey(int[] a){
-        int sum = 0;
+    public static boolean checkBalance(int[] a){
+        int sumAll = 0;
+        int sumEnd = 0;
+        boolean check = false;
         for (int i = 0; i < a.length; i++) {
-            sum = sum + a[i];
+            sumAll = sumAll + a[i];
         }
         for (int j = 0; j < a.length; j++) {
-            if (sum - a[j] == a[j]) {
-                return true;
-            } else if (sum == 0) {
-                return false;
-            } else {
-                sum = sum - a[j];
+            sumAll = sumAll - a[j];
+            sumEnd = sumEnd + a[j];
+            if (sumAll == sumEnd) {
+                check = true;
+                break;
+            }else{
+                check = false;
             }
         }
+        return check;
+    }
+    // Задание 8
+    public static int[] mixingValuesInArray(int[] array, int a){
+        Stack stack = new Stack<>();
+        for (int i = 0; i < array.length; i++) {
+            stack.push(array[array.length -1 -i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            int t;
+            if(i + a >= 0 && i + a < array.length){
+                t = i + a;
+             }else if(i + a < 0) {
+                 t = array.length + i + a;
+            }else{
+                t = (i + a) - array.length;
+             }
+            array[t] = (int) stack.pop();
+        }
+        return array;
     }
 }
