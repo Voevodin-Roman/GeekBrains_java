653ef9db88e6407f0db377d4a195bb73050897ab
Voevodin_Roman <q556677@yandex.ru>
2022-04-28 13:30:09 +0400

    Add new class Employee Add homework 5 (task 1 - 5)

--- src/main/java/homework5/Employee.java
+++ src/main/java/homework5/Employee.java
@@ -7,12 +7,12 @@ public class Employee {
    private final String middle_name;
    private final String function;
    private final String email;
-   private final long phone;
+   private final String phone;
    private final int salary;
    private final int old;
 
    // Задание 2: создаем конструктор
-   public Employee(String name, String last_name, String middle_name, String function, String email, long phone, int salary, int old) {
+   public Employee(String name, String last_name, String middle_name, String function, String email, String phone, int salary, int old) {
       this.name = name;
       this.last_name = last_name;
       this.middle_name = middle_name;
@@ -40,11 +40,11 @@ public class Employee {
    public static void main(String[] args) {
       //Задание 4: создаём массив из объектов
       Employee[] arrayEmployees = new  Employee[5];
-      arrayEmployees[0] = new Employee("Иван", "Иванович", "Иванов", "Инженер", "ivanov@n.n", 79054851212L,150000, 30);
-      arrayEmployees[1] = new Employee("Петр", "Петров", "Пертович", "Престидижитатор", "petrov@p.p", 79065524212L, 200000, 45);
-      arrayEmployees[2] = new Employee("Сидор", "Сидоров", "Сидорович", "Дворник", "sidorov@s.s", 79030001214L,15000, 52);
-      arrayEmployees[3] = new Employee("Фекл", "Феклов", "Феклович", "Пахарь", "feklov@f.f", 78058654444L, 12563, 68);
-      arrayEmployees[4] = new Employee("Лентяй", "Лентяев", "Лентяевич", "Потомственный безработный", "lentayev@l.l", 79045556622L, 2, 25);
+      arrayEmployees[0] = new Employee("Иван", "Иванович", "Иванов", "Инженер", "ivanov@n.n", "+79054851212",150000, 30);
+      arrayEmployees[1] = new Employee("Петр", "Петров", "Пертович", "Престидижитатор", "petrov@p.p", "+79065524212", 200000, 45);
+      arrayEmployees[2] = new Employee("Сидор", "Сидоров", "Сидорович", "Дворник", "sidorov@s.s", "+79030001214",15000, 33);
+      arrayEmployees[3] = new Employee("Фекл", "Феклов", "Феклович", "Пахарь", "feklov@f.f", "+78058654444", 12563, 68);
+      arrayEmployees[4] = new Employee("Лентяй", "Лентяев", "Лентяевич", "Потомственный безработный", "lentayev@l.l", "+79045556622", 2000, 44);
 
       //Задание 5: печатаем сотрудников возрастом выше 40 лет
       for (int i = 0; i < arrayEmployees.length; i++) {
