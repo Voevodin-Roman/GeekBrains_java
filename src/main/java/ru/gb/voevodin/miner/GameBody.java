21c065bae83cbb20a8824269b3c7f6c0bca1bae7
Voevodin_Roman <q556677@yandex.ru>
2022-06-06 19:24:59 +0400

    add homework 8 100%

--- src/main/java/ru/gb/voevodin/miner/GameBody.java
+++ src/main/java/ru/gb/voevodin/miner/GameBody.java
@@ -1,24 +1,11 @@
 package ru.gb.voevodin.miner;
 import java.util.Random;
 public class GameBody {
-    private static final String ANSI_PURPLE = "\u001B[35m";
-    private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_BLUE = "\u001B[34m";
-    private static final String ANSI_GREY = "\u001b[38;5;236m";
-    private static final String ANSI_GREY_BACKGROUND = "\u001b[48;5;246m";
-    private static final String ANSI_BLACK_BACKGROUND = "\u001b[48;5;238m";
-    private static final String ANSI_RED_BACKGROUND = "\u001b[48;5;9m";
-    private static final String ANSI_YELLOW_BACKGROUND = "\u001b[48;5;226m";
+
     private static final int CELL_FLAG = 100000000;
     private final int width;
     private final int height;
     private final int mine;
-    private int multiplication;
     private static final int MINE = -10;
     private static final int EMPTY = -1;
 
@@ -26,35 +13,10 @@ public class GameBody {
         this.width = width;
         this.height = height;
         this.mine = mine;
-        multiplication = width * height;
 
     }
 
-
-    private static String getColorCode(final int i) {
-        switch (i) {
-            case EMPTY:
-                return ANSI_WHITE;
-            case 1:
-                return ANSI_BLUE;
-            case 2:
-                return ANSI_GREEN;
-            case 3:
-                return ANSI_RED;
-            case 4:
-                return ANSI_CYAN;
-            case 5:
-                return ANSI_YELLOW;
-            case 6:
-                return ANSI_PURPLE;
-        }
-        return ANSI_GREY;
-    }
-
-
-
-
-    public   int[][] fillStatus() {
+    public void fillStatus() {
         int[][] board = new int[width][height];
         int mines = mine;
         final Random random = new Random();
@@ -66,21 +28,17 @@ public class GameBody {
             board[x][y] = MINE;
             mines--;
         }
-
-
         board = calculateMines(board);
         GameLogic gameLogic = new GameLogic(board, width, height);
 
 
-        for (int i = 0; i < board.length; i++) {
+        for (int[] ints : board) {
             System.out.println();
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j]+ "   ");
+            for (int anInt : ints) {
+                System.out.print(anInt + "   ");
 
             }
-
         }
-        return board;
     }
     private int[][] calculateMines(int[][] board) {
         for (int i = 0; i < width; i++) {
--- src/main/java/ru/gb/voevodin/miner/GameLogic.java
+++ src/main/java/ru/gb/voevodin/miner/GameLogic.java
@@ -6,19 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameLogic extends JFrame{
-    private int[][] game_bord;
+
+    private final int[][] gameBord;
     private final int width;
     private final int height;
     private JButton[][] jButtons;
 
     public GameLogic(int[][] game_bord, int width, int height) {
-        this.game_bord = game_bord;
+        this.gameBord = game_bord;
         this.width = width;
         this.height = height;
         creatingArrayButton();
-
     }
-
 
     public void creatingArrayButton(){
         jButtons = new JButton[width][height];
@@ -40,35 +39,61 @@ public class GameLogic extends JFrame{
         setLayout(new GridLayout(width, height));
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                add(new JButton("0"));
+                add(jButtons[i][j]);
                 int finalI = i;
                 int finalJ = j;
-                jButtons[i][j].addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        jButtons[finalI][finalJ].setText("2");
+                jButtons[i][j].addActionListener(actionEvent -> {
+                    if(gameBord[finalI][finalJ] == -10){
+                        dispose();
+                        gameOver();
 
+                    }else{
+                        jButtons[finalI][finalJ].setText(getColorCode(gameBord[finalI][finalJ]));
                     }
                 });
-
             }
         }
-      //for (int i = 0; i < width; i++) {
-      //    for (int j = 0; j < height; j++) {
-      //        int finalI = i;
-      //        int finalJ = j;
-      //        jButtons[i][j].addActionListener(new ActionListener() {
-      //             @Override
-      //             public void actionPerformed(ActionEvent e) {
-      //                 jButtons[finalI][finalJ]= new JButton(String.valueOf(game_bord[finalI][finalJ]));
-
-
-      //            }
-      //        });
-      //    }//creatingGameWindow();
-      //}
         setVisible(true);
     }
+    private void gameOver(){
+        setTitle("Найди мины");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, width*60, height*60);
+        setLayout(new GridLayout(width, height));
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                    jButtons[i][j].setText(getColorCode(gameBord[i][j]));
+                    add(jButtons[i][j]);
+            }
+        }
+        setVisible(true);
+        JOptionPane.showMessageDialog(null, "Вы проиграли");
+        dispose();
+    }
+
+    private String getColorCode(final int i) {
+        switch (i) {
+            case 1:
+                return "<html><h2><font color=\"blue\">1";
+            case 2:
+                return "<html><h2><font color=\"green\">2";
+            case 3:
+                return "<html><h2><font color=\"red\">3";
+            case 4:
+                return "<html><h2><font color=\"cyan\">4";
+            case 5:
+                return "<html><h2><font color=\"yellow\">5";
+            case 6:
+                return "<html><h2><font color=\"purple\">6";
+            case 7:
+                return "<html><h2><font color=\"purple\">6";
+            case 8:
+                return "<html><h2><font color=\"purple\">6";
+            case -10:
+                return "<html><h2><font color=\"red\">@";
+        }
+        return " ";
 
 
+    }
 }
